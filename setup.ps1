2c5807ad791be59e493d4ef96e2a84d1a1b411b2
Justin Fu <69432667+jfu299@users.noreply.github.com>
2020-12-17 14:07:09 -0500

    Create setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,9 @@
+
+# setup.ps1
+# Custom Setup for Windows 10 (Windows 10 Version 1909/19H2 - OS Build 18363)
+# 		https://github.com/jfu299/win10setup
+# 		https://raw.githubusercontent.com/jfu299/win10setup/main/setup.ps1
+# By: Justin Fu
+# Updated: December 17, 2020
+
+Get-AppxPackage | where-object {$_.name -notlike "*store*"} | where-object {$_.name -notlike "*terminal*"} | where-object {$_.name -notlike "*calc*"} | Remove-AppxPackage
