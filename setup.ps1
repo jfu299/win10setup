7004e1f9a8944a250701ba358235ce87f42692fd
Justin Fu <69432667+jfu299@users.noreply.github.com>
2021-07-04 11:44:40 -0400

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,10 @@
 
 # setup.ps1
-# Custom Setup for Windows 10 (Windows 10 Version 1909/19H2 - OS Build 18363)
-#       https://github.com/jfu299/win10setup
-#       https://raw.githubusercontent.com/jfu299/win10setup/main/setup.ps1
+# Custom Setup for Windows 10
+# 		https://github.com/jfu299/win10setup
+# 		https://raw.githubusercontent.com/jfu299/win10setup/main/setup.ps1
 # By: Justin Fu
-# Updated: Janaury 2, 2021
+# Updated: July 4, 2021
 
 # --------------
 # Remove All UWP Apps (Except Microsoft Store, Windows Terminal, and Calculator)
@@ -12,5 +12,3 @@
 Get-AppxPackage | where-object {$_.name -notlike "*store*"} | where-object {$_.name -notlike "*terminal*"} | where-object {$_.name -notlike "*calc*"} | Remove-AppxPackage
 
 # --------------
-
-
