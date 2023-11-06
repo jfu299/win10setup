2a2173b966acd69e7ccb50a56e3efdbb8687056c
Justin Fu <69432667+jfu299@users.noreply.github.com>
2023-11-06 14:42:22 -0500

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -4,7 +4,7 @@
 # 		https://github.com/jfu299/win10setup
 # 		https://raw.githubusercontent.com/jfu299/win10setup/main/setup.ps1
 # By: Justin Fu
-# Updated: October 13, 2023
+# Updated: November 06, 2023
 
 # --------------
 # Remove All UWP Apps (Except Microsoft Store, Windows Terminal, and Calculator)
@@ -15,6 +15,14 @@ Get-AppxPackage | where-object {$_.name -notlike "*store*"} | where-object {$_.n
 
 # Remove Windows Backup App, UWP Snipping Tool App, UWP Emoji Picker
 
-Remove-WindowsPackage -Online -PackageName "Microsoft-Windows-UserExperience-Desktop-Package~31bf3856ad364e35~amd64~~10.0.19041.3393" -NoRestart
+Get-WindowsPackage -Online -PackageName *Microsoft-Windows-UserExperience-Desktop* | Remove-WindowsPackage -Online -NoRestart
+
+# Remove Quick Assist
+
+Get-WindowsPackage -Online -PackageName *Microsoft-Windows-QuickAssist* | Remove-WindowsPackage -Online -NoRestart
+
+# Remove Windows Hello Face
+
+Get-WindowsPackage -Online -PackageName *Microsoft-Windows-Hello-Face* | Remove-WindowsPackage -Online -NoRestart
 
 # --------------
