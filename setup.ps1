a0ed20df306997ae0600763f000e2432c35e2772
Justin Fu <69432667+jfu299@users.noreply.github.com>
2025-10-22 17:27:44 -0400

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,15 +1,22 @@
 
 # setup.ps1
 # Custom Setup for Windows 10
-# 		https://github.com/jfu299/win10setup
-# 		https://raw.githubusercontent.com/jfu299/win10setup/main/setup.ps1
+#       https://github.com/jfu299/win10setup
+#       https://raw.githubusercontent.com/jfu299/win10setup/main/setup.ps1
 # By: Justin Fu
-# Updated: May 16, 2024
+# Updated: 2025-10-22
 
 # --------------
 # Remove All UWP Apps (Except Microsoft Store, Windows Terminal, and Calculator)
+# !!!!!!! DO NOT USE ON WINDOWS 11 !!!!!!!!!!!!  ------ !!!!!!!!! WILL COMPLETELY BREAK THE DESKTOP AND OS !!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!! if you remove *cbs* from windows 11, it completely breaks the desktop and basic usage after restart !!!!!!!!!!!!!!!!
+# Get-AppxPackage | where-object {$_.name -notlike "*store*"} | where-object {$_.name -notlike "*terminal*"} | where-object {$_.name -notlike "*calc*"} | Remove-AppxPackage
+# --------------
 
-Get-AppxPackage | where-object {$_.name -notlike "*store*"} | where-object {$_.name -notlike "*terminal*"} | where-object {$_.name -notlike "*calc*"} | Remove-AppxPackage
+# Remove All UWP Apps (Except Microsoft Store, Windows Terminal, and Calculator)
+# Possible Fix for Windows 11
+Get-AppxPackage | where-object {$_.name -notlike "*control*"} | where-object {$_.name -notlike "*lock*"} | where-object {$_.name -notlike "*cbs*"} | where-object {$_.name -notlike "*CloudExperienceHost*"} | where-object {$_.name -notlike "*store*"} | where-object {$_.name -notlike "*terminal*"} | where-object {$_.name -notlike "*calc*"} | Remove-AppxPackage
+
 
 # -------
 
